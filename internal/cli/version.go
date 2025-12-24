e73c35bcb60d656353b0934f28ec7b2562bf5c75
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2025-12-24 16:01:33 +0100

    fix: version display now uses build-time ldflags

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -7,6 +7,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Build-time variables set via ldflags
+var (
+	version = "dev"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show version information",
@@ -23,7 +30,7 @@ func init() {
 func printVersionInfo() {
 	fmt.Fprintln(os.Stderr, asciiLogo)
 	fmt.Fprintln(os.Stderr)
-	fmt.Println("pgmi 0.1.0-MVP")
+	fmt.Printf("pgmi %s\n", version)
 	fmt.Fprintln(os.Stderr, "PostgreSQL deployment tool")
 	fmt.Fprintln(os.Stderr)
 	fmt.Fprintln(os.Stderr, "Repository: https://github.com/vvka-141/pgmi")
